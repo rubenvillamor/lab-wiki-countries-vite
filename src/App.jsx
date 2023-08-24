babe8e05f8ab1ef5d60c7191d6be88066ad2ec7b
Ruben <villamorlage@gmail.com>
2023-08-24 19:52:40 +0200

    un poco

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import "./App.css";
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React WikiCountries</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>LAB | React WikiCountries</h1>
+      </div>
+      <nav>
+        <Link to="/">HomePage</Link>
+        <Link to="/someCountryId">CountryDetailsPage</Link> 
+      </nav>
+      console.log()
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,27 @@
-function Navbar() {}
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
-export default Navbar;
+function HomePage() {
+ 
+
+  const [countries, setCountries] =useState()
+
+  useEffect(()=>{
+    axios.get("https://ih-countries-api.herokuapp.com/countries")
+    .then((response)=>{
+      console.log(response)
+      setCountries(response.data.results)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+  },[])
+  return (
+    <div>WikiCountries: The guide</div>
+  )
+}
+
+export default HomePage
